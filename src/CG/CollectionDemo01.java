da25c83db244c4a49340ac97e4195ed76c1f728d
cg <2698678494@qq.com>
2021-09-12 16:18:26 +0800

    陈淦提交的代码

---
+++ src/CG/CollectionDemo01.java
@@ -0,0 +1,21 @@
+package CG;
+
+/*
+* 增加
+*   add(E e)将指定的元素添加到此列表的尾部。
+*   addAll(Collection<? extends E> c)将指定集合中的所有元素添加到此集合（可选操作）。
+* 删除
+*   clear()从此集合中删除所有元素（可选操作）。
+*   remove(Object o) 从该集合中删除指定元素的单个实例（如果存在）（可选操作）。
+* 修改
+* 遍历（查询）
+* 判断
+*   contains(Object o)如果此集合包含指定的元素，则返回 true 。
+*   containsAll(Collection<?> c)如果此集合包含指定 集合中的所有元素，则返回true。
+*   equals(Object o) 将指定的对象与此集合进行比较以获得相等性
+*   isEmpty()如果此集合不包含元素，则返回 true
+*
+ * */
+public class CollectionDemo01 {
+
+}
